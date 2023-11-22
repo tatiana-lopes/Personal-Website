19c6ca613717b7b9cbfa9fce1c2527c3efb9fa29
tatiana-lopes <92187392+tatiana-lopes@users.noreply.github.com>
2023-11-22 19:26:56 +0000

    Cleaned up the code

--- index.js
+++ index.js
@@ -4,16 +4,13 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { CSS3DRenderer } from 'three/addons/renderers/CSS3DRenderer.js';
 import { Sky } from 'three/addons/objects/Sky.js';
-// Create a renderer
+
+
 let renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById("Model3D").appendChild(renderer.domElement);
-
-// Create a scene
 let scene = new THREE.Scene();
-//add a gradient background to the scene
 scene.background = null
-
 // make the background transparent
 renderer.setClearColor(0x000000, 0);
 
@@ -21,9 +18,7 @@ const fov = 60;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 0.1;
 const far = 1000;
-
 let camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-// Set the camera position
 camera.position.set(0, 3, 6)
 scene.add(camera);
 
@@ -37,17 +32,13 @@ let aimTimeline = pageYOffset / 3000;
 const ambient = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambient);
 
-
-
 //create directional light towards the earth 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.9);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
 directionalLight.position.set(0, 0, 1);
 scene.add(directionalLight);
 
 let earth = null;
-
 const loader = new GLTFLoader();
-
 
 
 //store the text classes in an array
@@ -59,7 +50,6 @@ loader.load('./Assets/Tatiana_Earth_Final.gltf', function (gltf) {
     earth = gltf.scene;
     scene.add(earth);
   
-
     //adjust the scale  and the position of the earth if it is on a mobile device
     if (window.innerWidth < 600) {
         earth.scale.set(1.9, 1.9, 1.9);
@@ -82,18 +72,6 @@ loader.load('./Assets/Tatiana_Earth_Final.gltf', function (gltf) {
 });
 
 
-//const text1 = null;
-//text1 = document.getElementsById("text1");
-//const hiddenVisibleClasses = null;
-//hiddenVisibleClasses = text1.classList;
-
-//store the text classes in an array
-
-//textClasses = document.getElementsByClassName("text");
-
-
-
-
 // Create an animate function
 function animate() {
     requestAnimationFrame(animate);
@@ -102,107 +80,6 @@ function animate() {
     currentTimeline += (aimTimeline - currentTimeline) * 0.1;
     const rotationX = currentTimeline * Math.PI * 2;
     earth.rotation.set(rotationX,0,0);
-    //console.log(currentTimeline);
-   
-    //change the class of the text from visible to hidden as the user scrolls down
-    /*
-    if(currentTimeline < 0.03){
-        //textClasses[0].className = "text visible";
-      
-        console.log("earthRotation 1:" + rotationX );
-        //document.getElementById("text1").className = "text visible";
-        document.getElementById("text1").className = "text visible";
-
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text hidden";
-    }
-    
-
-    if(currentTimeline > 0.03 && currentTimeline < 0.16){
-        console.log("earthRotation 2:" + rotationX );
-        document.getElementById("text1").className = "text hidden";
-        document.getElementById("text2").className = "text visible";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text hidden";
-        document.getElementById("text8").className = "text hidden";
-    }
-    
-    if (currentTimeline > 0.16 && currentTimeline < 0.32){
-        console.log("earthRotation 3:" + rotationX );
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text visible";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text hidden";
-        document.getElementById("text8").className = "text hidden";
-
-    }
-
-    if (currentTimeline > 0.32 && currentTimeline < 0.48){
-        console.log("earthRotation 4:" + rotationX );
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text visible";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text hidden";
-        document.getElementById("text8").className = "text hidden";
-    }
-
-    if (currentTimeline > 0.48 && currentTimeline < 0.64){
-        console.log("earthRotation 5:" + rotationX );
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text visible";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text hidden";
-        document.getElementById("text8").className = "text hidden";
-    }
-
-    if (currentTimeline > 0.64 && currentTimeline < 0.8){
-        console.log("earthRotation 6:" + rotationX );
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text visible";
-        document.getElementById("text7").className = "text hidden";
-        document.getElementById("text8").className = "text hidden";
-    }
-
-    if (currentTimeline > 0.8 && currentTimeline < 0.96){
-        console.log("earthRotation 7:" + rotationX );
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text visible";
-        document.getElementById("text8").className = "text hidden";
-    }
-
-    if (currentTimeline > 0.96){
-        console.log("earthRotation 8:" + rotationX );
-        document.getElementById("text2").className = "text hidden";
-        document.getElementById("text3").className = "text hidden";
-        document.getElementById("text4").className = "text hidden";
-        document.getElementById("text5").className = "text hidden";
-        document.getElementById("text6").className = "text hidden";
-        document.getElementById("text7").className = "text hidden";
-        document.getElementById("text8").className = "text visible";
-    }
-    */
-    
-
     renderer.render(scene, camera);
 }
 
@@ -249,7 +126,6 @@ window.addEventListener("scroll",function(){
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 }
-
 
 // position the earth always to the center of the screen when the window is resized
 window.addEventListener("resize", function () {
